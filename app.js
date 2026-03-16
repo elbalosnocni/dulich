55a602c77cfd8b2e02d8203fc3c6319d9aa03f62
vtc <wuzhanggong@gmail.com>
2026-03-16 20:59:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const API_URL="https://script.google.com/macros/s/AKfycbzHR7iYAxL5JBpyJO5mzlZ2CP-CwE1rtjn2iREHbXAVuzr0BNn_P2ntesXk5_Wa0Mzv/exec"
+const API="https://script.google.com/macros/s/AKfycbzHR7iYAxL5JBpyJO5mzlZ2CP-CwE1rtjn2iREHbXAVuzr0BNn_P2ntesXk5_Wa0Mzv/exec"
+
+const search=document.getElementById("search")
+const result=document.getElementById("result")
 
 let nv=null
 
@@ -8,7 +11,7 @@ const q=this.value
 
 if(q.length<2) return
 
-const res=await fetch(API_URL+"?action=search&q="+q)
+const res=await fetch(API+"?action=search&q="+q)
 
 const data=await res.json()
 
@@ -17,7 +20,7 @@ let html=""
 data.slice(0,5).forEach(n=>{
 
 html+=`
-<div onclick='pick(${JSON.stringify(n)})' style="padding:8px;border-bottom:1px solid #eee;cursor:pointer">
+<div onclick='pick(${JSON.stringify(n)})'>
 ${n.ten} (${n.ma})
 </div>`
 
@@ -35,6 +38,59 @@ result.innerHTML=`
 <b>${n.ten}</b><br>
 ${n.bophan} - ${n.chucvu}
 `
+
+calc()
+
+}
+
+function calc(){
+
+if(!nv) return
+
+const adult=+document.getElementById("adult").value
+const child=+document.getElementById("child").value
+const family=+document.getElementById("family").value
+
+let price=0
+
+if(nv.congdoan=="Có"){
+price+=adult*1100000
+}else{
+price+=adult*2100000
+}
+
+price+=child*1550000
+price+=family*3100000
+
+document.getElementById("money").innerText=
+price.toLocaleString()+" đ"
+
+}
+
+async function register(){
+
+if(!nv){
+alert("Chọn nhân viên")
+return
+}
+
+const url=API+"?action=register"+
+"&ma="+nv.ma+
+"&ten="+encodeURIComponent(nv.ten)+
+"&gioitinh="+nv.gioitinh+
+"&congdoan="+nv.congdoan+
+"&adult="+adult.value+
+"&child="+child.value+
+"&family="+family.value+
+"&total="+money.innerText
+
+const res=await fetch(url)
+
+const text=await res.text()
+
+alert(text)
+
+}`
 
 calc()
 
