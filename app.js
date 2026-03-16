b6b5a72de60db158997c3ca929c91ff23127918c
vtc <wuzhanggong@gmail.com>
2026-03-16 20:47:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const q=this.value
 
 if(q.length<2) return
 
-const res=await fetch(https://script.google.com/macros/s/AKfycbzHR7iYAxL5JBpyJO5mzlZ2CP-CwE1rtjn2iREHbXAVuzr0BNn_P2ntesXk5_Wa0Mzv/exec+"?action=search&q="+q)
+const res=await fetch(API_URL+"?action=search&q="+q)
 
 const data=await res.json()
 
@@ -17,7 +17,7 @@ let html=""
 data.slice(0,5).forEach(n=>{
 
 html+=`
-<div onclick='pick(${JSON.stringify(n)})'>
+<div onclick='pick(${JSON.stringify(n)})' style="padding:8px;border-bottom:1px solid #eee;cursor:pointer">
 ${n.ten} (${n.ma})
 </div>`
 
@@ -35,6 +35,69 @@ result.innerHTML=`
 <b>${n.ten}</b><br>
 ${n.bophan} - ${n.chucvu}
 `
+
+calc()
+
+}
+
+function calc(){
+
+if(!nv) return
+
+let adult=+document.getElementById("adult").value
+let child=+document.getElementById("child").value
+let family=+document.getElementById("family").value
+
+let price=0
+
+if(nv.congdoan=="Có"){
+price+=adult*1100000
+}else{
+price+=adult*2100000
+}
+
+price+=child*1550000
+price+=family*3100000
+
+document.getElementById("money").innerText=
+price.toLocaleString()+" đ"
+
+}
+
+document.querySelectorAll("input").forEach(e=>{
+e.oninput=calc
+})
+
+async function register(){
+
+if(!nv){
+alert("Chọn nhân viên")
+return
+}
+
+const url=API_URL+"?action=register"+
+"&ma="+nv.ma+
+"&ten="+encodeURIComponent(nv.ten)+
+"&gioitinh="+nv.gioitinh+
+"&congdoan="+nv.congdoan+
+"&adult="+document.getElementById("adult").value+
+"&child="+document.getElementById("child").value+
+"&family="+document.getElementById("family").value+
+"&total="+document.getElementById("money").innerText
+
+const res=await fetch(url)
+
+const text=await res.text()
+
+if(text=="EXIST"){
+alert("Nhân viên đã đăng ký")
+}else if(text=="CLOSED"){
+alert("Đã hết hạn đăng ký")
+}else{
+alert("Đăng ký thành công")
+}
+
+}`
 
 calc()
 
