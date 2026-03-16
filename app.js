a663289868d4c2904b52cb0b66277b2d928fc487
vtc <wuzhanggong@gmail.com>
2026-03-17 06:41:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,24 @@ const API="https://script.google.com/macros/s/AKfycbzHR7iYAxL5JBpyJO5mzlZ2CP-CwE
 const search=document.getElementById("search")
 const result=document.getElementById("result")
 
+const adult=document.getElementById("adult")
+const child=document.getElementById("child")
+const family=document.getElementById("family")
+const money=document.getElementById("money")
+
 let nv=null
+
 
 search.oninput=async function(){
 
-const q=this.value
+const q=this.value.trim()
 
-if(q.length<2) return
+if(q.length<2){
+result.innerHTML=""
+return
+}
 
-const res=await fetch(API+"?action=search&q="+q)
-
+const res=await fetch(API+"?action=search&q="+encodeURIComponent(q))
 const data=await res.json()
 
 let html=""
@@ -22,13 +30,16 @@ data.slice(0,5).forEach(n=>{
 html+=`
 <div onclick='pick(${JSON.stringify(n)})'>
 ${n.ten} (${n.ma})
-</div>`
+</div>
+`
 
 })
 
 result.innerHTML=html
 
 }
+
+
 
 function pick(n){
 
@@ -43,152 +54,38 @@ calc()
 
 }
 
-function calc(){
 
-if(!nv) return
-
-const adult=+document.getElementById("adult").value
-const child=+document.getElementById("child").value
-const family=+document.getElementById("family").value
-
-let price=0
-
-if(nv.congdoan=="Có"){
-price+=adult*1100000
-}else{
-price+=adult*2100000
-}
-
-price+=child*1550000
-price+=family*3100000
-
-document.getElementById("money").innerText=
-price.toLocaleString()+" đ"
-
-}
-
-async function register(){
-
-if(!nv){
-alert("Chọn nhân viên")
-return
-}
-
-const url=API+"?action=register"+
-"&ma="+nv.ma+
-"&ten="+encodeURIComponent(nv.ten)+
-"&gioitinh="+nv.gioitinh+
-"&congdoan="+nv.congdoan+
-"&adult="+adult.value+
-"&child="+child.value+
-"&family="+family.value+
-"&total="+money.innerText
-
-const res=await fetch(url)
-
-const text=await res.text()
-
-alert(text)
-
-}`
-
-calc()
-
-}
 
 function calc(){
 
 if(!nv) return
 
-let adult=+document.getElementById("adult").value
-let child=+document.getElementById("child").value
-let family=+document.getElementById("family").value
+let a=+adult.value
+let c=+child.value
+let f=+family.value
 
 let price=0
 
 if(nv.congdoan=="Có"){
-price+=adult*1100000
+price+=a*1100000
 }else{
-price+=adult*2100000
+price+=a*2100000
 }
 
-price+=child*1550000
-price+=family*3100000
-
-document.getElementById("money").innerText=
-price.toLocaleString()+" đ"
-
-}
-
-document.querySelectorAll("input").forEach(e=>{
-e.oninput=calc
-})
-
-async function register(){
-
-if(!nv){
-alert("Chọn nhân viên")
-return
-}
-
-const url=API_URL+"?action=register"+
-"&ma="+nv.ma+
-"&ten="+encodeURIComponent(nv.ten)+
-"&gioitinh="+nv.gioitinh+
-"&congdoan="+nv.congdoan+
-"&adult="+document.getElementById("adult").value+
-"&child="+document.getElementById("child").value+
-"&family="+document.getElementById("family").value+
-"&total="+document.getElementById("money").innerText
-
-const res=await fetch(url)
-
-const text=await res.text()
-
-if(text=="EXIST"){
-alert("Nhân viên đã đăng ký")
-}else if(text=="CLOSED"){
-alert("Đã hết hạn đăng ký")
-}else{
-alert("Đăng ký thành công")
-}
-
-}`
-
-calc()
-
-}
-
-function calc(){
-
-if(!nv) return
-
-let adult=+adult.value
-let child=+child.value
-let family=+family.value
-
-let price=0
-
-if(nv.congdoan=="Có"){
-
-price+=adult*1100000
-
-}else{
-
-price+=adult*2100000
-
-}
-
-price+=child*1550000
-price+=family*3100000
+price+=c*1550000
+price+=f*3100000
 
 money.innerText=price.toLocaleString()+" đ"
 
 }
 
+
+
 document.querySelectorAll("input").forEach(e=>{
 e.oninput=calc
 })
+
+
 
 async function register(){
 
@@ -197,7 +94,8 @@ alert("Chọn nhân viên")
 return
 }
 
-const url=https://script.google.com/macros/s/AKfycbzHR7iYAxL5JBpyJO5mzlZ2CP-CwE1rtjn2iREHbXAVuzr0BNn_P2ntesXk5_Wa0Mzv/exec+"?action=register"+
+const url=API+
+"?action=register"+
 "&ma="+nv.ma+
 "&ten="+encodeURIComponent(nv.ten)+
 "&gioitinh="+nv.gioitinh+
@@ -213,9 +111,11 @@ const text=await res.text()
 
 if(text=="EXIST"){
 alert("Nhân viên đã đăng ký")
-}else if(text=="CLOSED"){
+}
+else if(text=="CLOSED"){
 alert("Đã hết hạn đăng ký")
-}else{
+}
+else{
 alert("Đăng ký thành công")
 }
 
